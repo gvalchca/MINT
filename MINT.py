b03139f405f5c74585ec75f86e3adebf146c56fe
maciejj <maciejj@cent.uw.edu.pl>
2018-01-19 14:27:49 -0500

    modified:   MINT.py

--- MINT.py
+++ MINT.py
@@ -19,6 +19,9 @@
 #*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
 #*                                                                          *
 #****************************************************************************/
+
+#github change 
+
 # !/usr/bin/python
 import gzip
 import math
